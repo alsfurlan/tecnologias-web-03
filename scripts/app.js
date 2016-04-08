9f2756d242ccc98bfcb10177ff4b2a007d84ee47
Anderson Luís Furlan <anderson-furlan@hotmail.com>
2016-04-08 15:37:43 -0300

    Manipulação do DOM - Eventos e sua execução assíncrona

--- scripts/app.js
+++ scripts/app.js
@@ -1,22 +1,6 @@
 
 $(document).ready(function() {
-   var $novaUL = $("<ul>");
-   var $novoParagrafo = $("<p>");
-   $novoParagrafo.text("Este é um parágrafo");
-   $("footer").append($novoParagrafo);
-   
-   var $li1 = $("<li>").text("Primeiro item");
-   var $li2 = $("<li>").text("Segundo item");
-   var $li3 = $("<li>").text("Terceiro item");
-   
-   $novaUL.append($li1);
-   $novaUL.append($li2);
-   $novaUL.append($li3);
-   
-   $("main").append($novaUL);
-   
-   var $rodapePrimeiroFilho  = $("<p>").text("Sou o primeiro elemento filho de footer");
-   $rodapePrimeiroFilho.appendTo($("footer"));
-   
-   $novaUL.fadeOut();
+   $('.duploClique').on('dblclick', function() {
+       window.alert('Duplo clique!');
+   });
 });
