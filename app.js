9a78d7ac17b94bde9acafd3e4347a1908711e0f5
Anderson Luís Furlan <anderson-furlan@hotmail.com>
2016-04-08 03:06:30 -0300

    Mais manipulação do DOM - Corrigindo bug - Limpando a caixa de texto de escrita de comentários

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ var app = function() {
         var $input = $('.escrever-comentario input'); // Campo de texto
         var comentario = $input.val(); // Pega o valor digitado
 
-        if (comentario !== "") {
+        if (comentario !== '') {
             var $novoComentario = $('<p>'); // Cria um novo objeto jQuery de parágrafo 
             $novoComentario.text(comentario); // Coloca no parágrafo o texto digitado 
             var $comentarios = $('.comentarios'); // objeto jQuery de seção de comentários
             $comentarios.append($novoComentario); // Adiciona ao final da seção o novo parágrafo com o comentário
+            $input.val('');
         }
     });
 };
