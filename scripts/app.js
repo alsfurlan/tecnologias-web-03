a29716132433a7a8223c59fa9744b2900da00f95
Anderson Luís Furlan <anderson-furlan@hotmail.com>
2016-04-08 15:13:00 -0300

    Manipulação do DOM

--- scripts/app.js
+++ scripts/app.js
@@ -4,4 +4,12 @@ $(document).ready(function() {
    var $novoParagrafo = $("<p>");
    $novoParagrafo.text("Este é um parágrafo");
    $("footer").append($novoParagrafo);
+   
+   var $li1 = $("<li>").text("Primeiro item");
+   var $li2 = $("<li>").text("Segundo item");
+   var $li3 = $("<li>").text("Terceiro item");
+   
+   $novaUL.append($li1);
+   $novaUL.append($li2);
+   $novaUL.append($li3);   
 });
