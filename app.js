25df260115d7945adf482744db15dc0e01d810c3
Anderson Luís Furlan <anderson-furlan@hotmail.com>
2016-04-08 03:00:19 -0300

    Validação de campos - Corrigindo bug de inserir comentário vazio

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 var app = function() {
     'use strict'
-    
+
     $('.escrever-comentario button').on('click', function(event) {
-    	var $input = $('.escrever-comentario input'); // Campo de texto
-    	var comentario = $input.val();			// Pega o valor digitado
-		var $novoComentario = $('<p>');			// Cria um novo objeto jQuery de parágrafo 
-		$novoComentario.text(comentario);		// Coloca no parágrafo o texto digitado 
-    	var $comentarios = $('.comentarios');	// objeto jQuery de seção de comentários
-    	$comentarios.append($novoComentario);	// Adiciona ao final da seção o novo parágrafo com o comentário
+        var $input = $('.escrever-comentario input'); // Campo de texto
+        var comentario = $input.val(); // Pega o valor digitado
+
+        if (comentario !== "") {
+            var $novoComentario = $('<p>'); // Cria um novo objeto jQuery de parágrafo 
+            $novoComentario.text(comentario); // Coloca no parágrafo o texto digitado 
+            var $comentarios = $('.comentarios'); // objeto jQuery de seção de comentários
+            $comentarios.append($novoComentario); // Adiciona ao final da seção o novo parágrafo com o comentário
+        }
     });
 };
 
