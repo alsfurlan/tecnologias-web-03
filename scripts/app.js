3df7d31f610ffed38d72c2041c702a8b3d1e0fef
Anderson Luís Furlan <anderson-furlan@hotmail.com>
2016-04-08 15:54:19 -0300

    Efeitos assíncronos e funções de callback

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,16 @@
 
-var main = function() {
+var main = function () {
     'use strict';
-    
+
     var $helloWorld = $("<div>").text("Hello world!").hide();
     var $goodbyeWorld = $("<div>").text("Goodbye world!").hide();
     $("body").append($helloWorld);
-    $helloWorld.slideDown(200);
-    //Adicionar "Goodbye World!" após a execução de slideDown
-    $("body").append($goodbyeWorld);
-    $goodbyeWorld.fadeIn();
+    $helloWorld.slideDown(2000, function () {
+        //Adicionar "Goodbye World!" após a execução de slideDown
+        $("body").append($goodbyeWorld);
+        $goodbyeWorld.fadeIn();
+    });
+
 }
 
 $(document).ready(main);
