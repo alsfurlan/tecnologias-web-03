9d8a9c9e8241d869db94b98d25ecd6b99dfc1d3b
Anderson Luís Furlan <anderson-furlan@hotmail.com>
2016-04-08 16:34:46 -0300

    Estrutura de decisão

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,42 @@
-$(document).ready(function(){
+$(document).ready(function() {
     'use strict';
-    
-    var nota = parseFloat(prompt("Qual a sua nota?"));
-    
-    if(nota === 10) {
-       window.alert('Parabéns você tirou a nota máxima!'); 
-    } else if (nota == 10) {
-        window.alert('Parabéns você tirou a nota máxima! [2]'); 
-    } else if(nota >= 9 && nota < 10) {
-       window.alert('Muito bom! Parabéns!');
-    } else if(nota >= 7 && nota < 9) {
-       window.alert('Bom!'); 
-    } else if(nota >= 6 && nota < 7) {
-       window.alert('Regular'); 
-    } else {
-        window.alert('Ruim! Estude mais da próxima vez!');
+
+    var nota = parseInt(prompt("Qual a sua nota?"));
+
+    switch (nota) {
+        case 10:
+            notaMaxima();
+            break;
+        case 9:
+            muitoBom();
+            break;
+        case 8:
+            bom();
+            break;
+        case 7:
+            bom();
+        case 6:
+            regular();
+        default:
+            estudeMais();
     }
+
+
+    function notaMaxima() {
+        window.alert('Parabéns você tirou a nota máxima!');
+    }
+
+    function muitoBom() {
+        window.alert('Muito bom! Parabéns!');
+    }
+    function bom() {
+    	window.alert('Bom!');
+    }
+	function regular() {
+	    window.alert('Regular');
+	}
+	function estudeMais() {
+    	window.alert('Ruim! Estude mais da próxima vez!');
+	}
+
 });
